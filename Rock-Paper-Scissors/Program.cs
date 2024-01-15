a7338cba22a5e232df13c2f04a6a0a1ae816c522
uomma <marcelloleone975@gmail.com>
2024-01-15 12:10:51 +0100

    player & cpu  choice

--- Rock-Paper-Scissors/Program.cs
+++ Rock-Paper-Scissors/Program.cs
@@ -10,6 +10,42 @@ namespace Rock_Paper_Scissors
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            bool PlayAgain = true;
+            String player;
+            String cpu ;
+
+            while (PlayAgain)
+            {
+                player = "";
+                cpu = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine("enter ROCK, PAPER, SCISSORS ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                    
+                }
+
+                switch (rand.Next(1, 4))
+                {
+                    case 1:
+                        cpu = "ROCK";
+                        break;
+                    case 2:
+                        cpu = "PAPER";
+                        break;
+                    case 3:
+                        cpu = "SCISSORS";
+                        break;
+                }
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Cpu: " + cpu);
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
